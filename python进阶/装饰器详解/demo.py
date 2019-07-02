0eb4c4e676bfde860c65f1e922b38c7935f73db3
djl <jialiang_ding@163.com>
2019-07-02 09:43:56 +0800

    s

---
+++ python进阶/装饰器详解/demo.py
@@ -0,0 +1,26 @@
+import logging
+def foo():
+    print('i am foo')
+#现在需要在函数中增加日志
+
+def foo2():
+    print("i am foo2")
+    logging.info("foo2 running")
+
+def use_logging(func):
+    logging.warn("%s is running" %func.__name__ )
+    func()
+
+def use_logging2(func):
+    def wrapper(*args,**kwargs):
+        logging.warn("%s is running" %func.__name__ )
+        return func(*args)
+    return wrapper
+@use_logging2
+def  foo3():
+     print("3333")
+
+
+if __name__ == "__main__":
+
+    foo3()
