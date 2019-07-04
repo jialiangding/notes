6fbb840b0936ffce683d9e509c9a9f97001b1c99
djl <jialiang_ding@163.com>
2019-07-04 13:57:28 +0800

    s

--- python进阶/高级特性/get_gentertor.py
+++ python进阶/高级特性/get_gentertor.py
@@ -12,3 +12,17 @@ for k, v in d.items():
 
 
 print([k + '=' + v for k, v in d.items()])
+
+
+L1 = ['Hello', 'World', 18, 'Apple', None]
+
+#[s.lower() for s in L1] 
+# 发生异常: AttributeError
+# 'int' object has no attribute 'lower'
+L2=[s.lower() for s in L1 if  isinstance(s, str)]
+print(L2)
+L3=2
+p = [x for x in range(1,101) if x%3 == 0]
+#1-100中，不是3的倍数的数取相反数，其余的数保持不变
+q = [x if x%3==0 else -x for x in range(1,101)]
+print(q)
