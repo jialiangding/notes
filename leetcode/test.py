858c84258be7f5a6d540df43c83b494f1a8096bb
djl <jialiang_ding@163.com>
2019-08-12 18:09:46 +0800

    4

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,3 +1,28 @@
 def s_a(number):
     
     pass
+
+
+
+
+def removeDuplicates(self, nums):
+
+
+
+    pass
+    
+#给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。+
+    def generate(self, numRows):
+         """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        result = []
+        for i in range(numRows):
+            now = [1]*(i+1)
+            if i >= 2:
+                for n in range(1,i):
+                    now[n] = pre[n-1]+pre[n]
+            result += [now]
+            pre = now
+        return result
