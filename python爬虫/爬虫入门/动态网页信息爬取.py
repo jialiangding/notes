8934638e6b2de90662f3a9d85821f774795a08e2
jeremy_mac <jialiang_ding@163.com>
2019-08-20 01:04:49 +0800

    s

--- python爬虫/爬虫入门/动态网页信息爬取.py
+++ python爬虫/爬虫入门/动态网页信息爬取.py
@@ -16,20 +16,22 @@ resp=requests.get("https://www.dianping.com/search/keyword/3/0_烧烤",headers={
 print(resp) #200
 
 from selenium import webdriver
-
+import os
 
 driver=webdriver.Chrome()
-driver.get("https://www.dianping.com/search/keyword/3/0_烧烤")
-print(driver.page_source)
+# driver.get("https://www.dianping.com/search/keyword/3/0_烧烤")
+# print(driver.page_source)
 
-driver.quit()
+# driver.quit()
 driver.get("https://www.baidu.com/")
 driver.find_element_by_id("kw").send_keys("iphone")
 driver.find_element_by_id("su").click()
 import time
 time.sleep(3)
+abspath = os.getcwd()  
+print(abspath)
 driver.save_screenshot("python爬虫/src")
 print(driver.current_url)
 # for elem in driver.find_element_by_xpath("//*[contains(@class,'result')]/h3/a")
 #   print(elem.text)
-driver.quit
+driver.quit()
