7b914552703717dafbdc0b77c6003f65fa641612
djl <jialiang_ding@163.com>
2019-08-20 18:25:42 +0800

    s

--- python爬虫/爬虫入门/chormehadless.py
+++ python爬虫/爬虫入门/chormehadless.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 
-chrome_driver="E:\notes\python爬虫\driver\chromedriver.exe"
+chrome_driver="C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 # chrome_options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(options=chrome_options,executable_path=chrome_driver)
 driver.get("https://www.baidu.com")
 print(driver.current_url)
+
+driver.find_element_by_id("kw").send_keys("iphone")
+driver.find_element_by_id("su").click()
 driver.close()
