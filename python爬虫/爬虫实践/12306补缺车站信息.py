b6ec12300710a0f47920f5e818a5309764b764db
djl <jialiang_ding@163.com>
2019-08-23 13:15:16 +0800

    sss

--- python爬虫/爬虫实践/12306补缺车站信息.py
+++ python爬虫/爬虫实践/12306补缺车站信息.py
@@ -1,4 +1,18 @@
-import requests
 
-resp=requests.get("https://www.12306.cn/index/index.html")
-print(resp.text)
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+import requests
+import os
+import re
+import io
+import sys
+
+chrome_driver="C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(options=chrome_options,executable_path=chrome_driver)
+start_url="https://www.12306.cn/index/script/core/common/station_name_v10036.js"
+driver.get(start_url)
+print(driver.page_source)
