c8cf1bc570ea2acec28de69449f0569c43507f47
dingjialiang <jialiang_ding@163.com>
2019-11-21 18:13:13 +0800

    1

---
+++ python进阶/接口自动化/douban.py
@@ -0,0 +1,38 @@
+import requests
+from requests.exceptions import  RequestException
+from bs4 import  BeautifulSoup
+import  lxml
+import  re
+
+
+
+
+def start_requests(url):
+        headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.146 Safari/537.36'}
+        response=requests.get(url,headers=headers)
+        html=response.text
+        soup=BeautifulSoup(html,"lxml")
+        #1.所有电影信息存储在dl标签
+        dlObj = soup.find('dl',class_='board-wrapper')
+        # 2.获取每个电影的详细信息，存储在li标签
+        details = dlObj.find_all('dd')
+        # 3.获取需要的电影信息
+        for detail in details:
+                # 电影名称
+                movieId = detail.find('i',class_='board-index').get_text()
+                movieName=detail.find('img',class_='board-img').get('alt')
+                moviePic=detail.find('img',class_='board-img').get('data-src')
+                movieStar1=detail.find('i',class_='integer').get_text()
+                movieStar2=detail.find('i',class_='fraction').get_text()
+                movieStar=movieStar1+movieStar2
+                print(movieStar)
+        next_page=
+
+def start_requests2(self):
+    for url in self.start_urls:
+        #  yield scrapy.Request(next_page_url, callback=self.parse)
+
+
+if __name__ == '__main__':
+   start_requests("https://maoyan.com/board/4?offset=0")
+    
--- python进阶/接口自动化/request.py
+++ python进阶/接口自动化/request.py
@@ -1,1 +1,60 @@
-import requests
+import  requests
+from requests.exceptions import  RequestException
+from bs4 import  BeautifulSoup
+import  lxml
+import  re
+def get_one_page(url):
+    try:
+         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) ''Chrome/51.0.2704.63 Safari/537.36'}
+         response=requests.get(url,headers=headers)
+         if response.status_code==200:
+          return  response.text
+    except RequestException:
+        return  None
+
+
+def parse_one_page(html):
+    # patten=re.compile('')
+#     # item=re.findall(patten,html)
+#     # print(item)
+     soup=BeautifulSoup(html,'lxml')
+     
+     soup2 = soup.find_all('dd')
+     print(soup2)
+#     images=soup.find_all( class_ ="board-img")
+#     for item in images:
+#         print(item.attrs['alt'])
+#         print(item.attrs['data-src'])
+    #  items= soup.find_all( class_ ="board-wrapper")
+    #  for item in items:
+    #      print(item)
+    #      num=item.find('i').text
+    #      # movie_name=item.find('img',class_='board-img"').attrs['alt']
+    #      # pic=item.find('img',class_='board-img"').attrs['data-src']
+    #      # print(movie_name)
+    #      # print(pic)
+    #      print(num)
+
+
+
+
+
+
+
+#
+#
+# for child in content.children:
+#     if child.string is not None:
+#         print(child.string.strip())
+
+def main(url):
+
+    html=get_one_page(url)
+    #print(html)
+
+
+if __name__ == '__main__':
+    url = "http://maoyan.com/board/4?offset=0"
+    html=get_one_page(url)
+   # parse_one_page()
+    parse_one_page(html)
